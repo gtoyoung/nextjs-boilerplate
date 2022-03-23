409a80f640d620d5c7544b0a8a786f0c3a942f11
dovb <gtoyoung@naver.com>
2022-03-24 08:58:05 +0900

    수정

--- components/sample/sample.tsx
+++ components/sample/sample.tsx
@@ -1,12 +1,4 @@
-import {
-  AspectRatio,
-  Box,
-  Button,
-  Flex,
-  Image,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Divider, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import { NasaPicture } from "services/sample.types";
 
@@ -19,9 +11,17 @@ const Sample = ({ sample }: { sample: NasaPicture[] }) => {
         </Text>
         {sample.map((item, index) => {
           return (
-            <AspectRatio key={index} maxW={400} ratio={4 / 3}>
-              <Image src={item.url} alt={item.title} objectFit={"cover"} />
-            </AspectRatio>
+            <>
+              {/* <AspectRatio ratio={1}> */}
+              <Image
+                key={index}
+                src={item.url}
+                alt={item.title}
+                objectFit={"cover"}
+              />
+              {/* </AspectRatio> */}
+              <Divider orientation="horizontal" />
+            </>
           );
         })}
       </VStack>
